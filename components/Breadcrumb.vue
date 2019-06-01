6df6e0a17ff82f2be8f4dae82c0d7784e2cbc989
guxukai <1378026744@qq.com>
2019-06-01 08:49:35 +0800

    tailwind components

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -5,9 +5,9 @@
         <span
           v-if="item.redirect === 'noRedirect' || index == levelList.length - 1"
           class="no-redirect"
-          >{{ item.meta.title }}</span
+          >{{ item.name }}</span
         >
-        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+        <a v-else @click.prevent="handleLink(item)">{{ item.name }}</a>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
@@ -33,27 +33,21 @@ export default {
   methods: {
     getBreadcrumb() {
       // only show routes with meta.title
-      let matched = this.$route.matched.filter(
-        item => item.meta && item.meta.title
-      )
+      let matched = this.$route.matched.filter(item => item.name)
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/dashboard', meta: { title: 'Dashboard' } }].concat(
-          matched
-        )
+        matched = [{ path: '/', name: 'index' }].concat(matched)
       }
 
-      this.levelList = matched.filter(
-        item => item.meta && item.meta.title && item.meta.breadcrumb !== false
-      )
+      this.levelList = matched.filter(item => item.name)
     },
     isDashboard(route) {
       const name = route && route.name
       if (!name) {
         return false
       }
-      return name.trim().toLocaleLowerCase() === 'Dashboard'.toLocaleLowerCase()
+      return name.trim().toLocaleLowerCase() === 'index'.toLocaleLowerCase()
     },
     pathCompile(path) {
       // To solve this problem https://github.com/PanJiaChen/vue-element-admin/issues/561
